f0182c441f36688ebd9b44e47ef7ceac9f8372b4
JetTheDog <79424948+JetTheDog@users.noreply.github.com>
2022-03-30 18:01:44 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,126 @@
+package application;
+	
+import static java.util.stream.Collectors.toMap;
+import java.util.Map;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import javafx.beans.binding.Bindings;
+import java.util.Scanner;
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.MapValueFactory;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.collections.FXCollections;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Main extends Application {
+	
+	// Word/Count data list
+	static Map<String, Integer> wordFrequency;
+	// Inital array of words
+	static ArrayList<String> poemWords = new ArrayList<String>();
+	
+	@Override
+	public void start(Stage primaryStage) {
+		
+		VBox pageVert = new VBox();
+		pageVert.prefHeightProperty().bind(primaryStage.heightProperty().multiply(0.80));
+		
+		Scene pageScene = new Scene(pageVert,700,600);
+		primaryStage.setScene(pageScene);
+		Label pageLabel = new Label("Poem total words: " + poemWords.size() + ", Unique words: " + wordFrequency.size());
+		TableView tableView = new TableView();
+
+		// Create class to manage columns, pass it word list/count
+		MapTable mapTable = new MapTable(wordFrequency);
+		tableView = mapTable.getTableView();
+		
+		tableView.setFixedCellSize(25);
+	    tableView.prefHeightProperty().bind(tableView.fixedCellSizeProperty().multiply(Bindings.size(tableView.getItems()).add(0.9)));
+		// Show it
+		pageLabel.setFont(new Font("Calibri", 24));
+		primaryStage.setScene(pageScene);
+		primaryStage.show();
+		
+		pageVert.getChildren().add(pageLabel);
+		pageVert.getChildren().add(tableView);	
+	}
+	
+	public static void main(String[] args) throws IOException {	
+		Boolean ret = evaluatePoem("ravenPoem.html");
+		
+		// Creates UI
+		if (ret)
+			launch(args);
+	}
+		
+	public static Boolean evaluatePoem(String poemFileName) throws IOException  {
+		// Local data
+		File poemFile = new File(poemFileName);
+		FileInputStream fis = new FileInputStream(poemFile);
+		byte[] data = new byte[(int) poemFile.length()];
+		Boolean retVal = false;
+		
+		try {
+			// Read file
+			fis.read(data);
+			fis.close();
+			String entirePoem = new String(data, "UTF-8");
+			int firstSpot = entirePoem.indexOf("<h1>"); // Start here
+			if (firstSpot == -1)
+				firstSpot = 0;
+			int lastSpot = entirePoem.indexOf("<!--end chapter-->"); // End here
+			if (lastSpot == -1)
+				lastSpot = entirePoem.length() - 1;
+			entirePoem = entirePoem.substring(firstSpot, lastSpot); // Shortens the page to just the poem
+			
+			// Remove unwanted chars
+			entirePoem = entirePoem.replaceAll("\\<.*?>",""); // Removes HTML tags
+			entirePoem = entirePoem.replaceAll("\\.",""); // Removes period
+			entirePoem = entirePoem.replaceAll("!"," "); // Removes !
+			entirePoem = entirePoem.replaceAll(","," "); // Removes ,
+			entirePoem = entirePoem.replaceAll(";"," "); // Removes ;
+			entirePoem = entirePoem.replaceAll("\\?"," "); // Removes ?
+			entirePoem = entirePoem.replaceAll("\\n"," "); // Removes \n
+			entirePoem = entirePoem.replaceAll("\\r"," "); // Removes \r
+			entirePoem = entirePoem.replaceAll("[^\\p{ASCII}]", " "); // Removes non-ASII
+			
+			// Collect just words
+			Scanner scanPoem = new Scanner(entirePoem);
+			scanPoem.useDelimiter(" |\\n|-"); // White space
+			while(scanPoem.hasNext()) {
+				String word = scanPoem.next();
+				if(word.length() > 0)
+					poemWords.add(word);
+			}
+			scanPoem.close();
+			
+			// Create count array
+			wordFrequency = poemWords.stream().collect(toMap(s -> s, s -> 1, Integer::sum)); // Maps words and counts occurrences
+			
+			retVal = true;
+
+			// Debugging
+			System.out.println("Sorted words:");
+			System.out.println(wordFrequency);
+		   
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		return retVal;
+	}
+}
